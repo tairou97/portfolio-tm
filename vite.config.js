a469c48ff1ed0be3548400cba2165411e7c5ae39
Tairou Mouhamed <tairou97-mouhamed@hotmail.com>
2024-03-03 00:19:54 +0100

    change

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    port: 3000, // Legt den Port für den Entwicklungsserver fest
+    open: true,
+    fs: {
+      // Umleitung aller Anfragen auf die Wurzel-URL auf die index.html-Datei
+      strict: false,
+      allow: ["/"],
+    },
+  },
+});
